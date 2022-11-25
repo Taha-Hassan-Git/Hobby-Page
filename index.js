382db2c87da899dfabcc3cd028e3ad3b558255b5
Taha Hassan <tahafhassan@gmail.com>
2022-11-25 11:09:07 +0000

    Content and styles updated

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
-today = mm + '/' + dd + '/' + yyyy;
+today = dd + '/' + mm + '/' + yyyy;
+
+trainingtime = yyyy - 2016;
 
 document.getElementById('date-time').innerHTML=today;
+document.getElementById('trainingtime').innerHTML=trainingtime;
